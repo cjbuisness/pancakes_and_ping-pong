7d376691fc411eefd3b5bb068d96117680e4bd37
cjbuis <cjbusinessadventure@gmail.com>
2024-11-02 18:21:23 -0700

    loading page time reduced

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function handleThanksgivingAnimation() {
         setTimeout(() => {
             thanksgivingElement.style.display = 'none'; // Remove from the DOM
         }, 500); // Wait for the fade-out to complete
-    }, 2000); // Change the time as needed
+    }, 1800); // Change the time as needed
 }
 
 // Initialize everything after the page is loaded
